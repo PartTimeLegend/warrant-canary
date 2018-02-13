9adbcc9c559dde94bfb8d8914c28b2040d0314b8
Antony Bailey <support@antonybailey.net>
2018-02-13 11:47:09 +0000

    Update checksignature.sh

--- checksignature.sh
+++ checksignature.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
-print "Fetching public key 0xd18da83cf2e50027 from keyservers\n"
-gpg --keyserver pool.sks-keyservers.net --recv-keys 0xd18da83cf2e50027 
+print "Fetching public key 0x9C02D726E567EF8B from keyservers\n"
+gpg --keyserver pool.sks-keyservers.net --recv-keys 0x9C02D726E567EF8B
 print "Show fingerprint of key to check\n"
-gpg --fingerprint 0xd18da83cf2e50027 
+gpg --fingerprint 0x9C02D726E567EF8B
 print "Verifying canary from git\n"
 gpg --verify canary.asc
 print "Downloading canary from website\n"
